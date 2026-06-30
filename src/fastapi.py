028e5915c66150f9a513dcc795d24699af655d51
Denis Smirnov <upccore@list.ru>
2026-06-30 15:15:09 +0300

    Add app factory and entry point

---
+++ src/fastapi.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+from src.presentation.api.routes.orders import router as orders_router
+
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+    app.include_router(orders_router)
+    return app
